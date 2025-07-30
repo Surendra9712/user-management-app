c82e64124ed776538fcd71f897ec1d0b14330ef1
Surendra Rawal <surendra.rawal@ensue.com.np>
2025-07-31 01:06:32 +0545

    google font remooved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,10 @@
 import type {Metadata} from "next";
-import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import  "@surendra9712/ui-components/dist/style.css";
 import {AppSessionProvider, AuthProvider} from "@/context/AuthProvider";
 import Layout from "@/components/layout/Layout";
 import {MessageProvider} from "@/context/MessageProvider";
 import {QueryProvider} from "@/context/QueryClientProvider";
-
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
 
 export const metadata:Metadata = {
     title: {
@@ -33,7 +22,6 @@ export default function RootLayout({
         <html lang="en">
         <body
             suppressHydrationWarning
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
         <AppSessionProvider>
             <QueryProvider>
